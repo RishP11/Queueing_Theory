f4b1ab6c57c60db33882291f26a010936dfe9da2
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-11-10 08:05:33 +0530

    restructuring the assignment

---
+++ coding_assignment/queue_models.py
@@ -0,0 +1,275 @@
+import numpy as np
+
+def m_m_1_q(simTime, del_t, arrRate, depRate):
+    """ 
+    Function to simulate an M/M/1 queue.
+    Just input 
+    1. simTime = Time duration (in seconds) that you would like to simulate the queue for.
+    2. arrRate = The arrival rate (per unit time)
+    3. depRate = The departure rate (per unit time)
+
+    You will get as output 3 lists.
+    1. The state of the queue at time t = 0, delT, 2delT, 3delT, and so on.
+    2. The interarrival times.
+    3. The departure times.   
+    4. The individual "waiting times".(i.e., the time required to wait in the queue + the time to get served). 
+    """ 
+    # To store the inter-arrival and inter-departure times
+    intArrTimes = []
+    intDepTimes = [] 
+    
+    # Timers for arrival and departure
+    arrTimer = 0 
+    depTimer = 0 
+
+    # Start with an empty queue :: Stores the state of the queue at [t=0, t=delT, t=2delT, t=3delT, ...]
+    num_runs = int(simTime / del_t)
+    stateHistory = np.zeros(num_runs)            
+    
+    # Individual customer timer and tracking ID (ID corresponds to the one being served).
+    individualTimers = []
+    customerID = 0                                
+
+    for i in range(1, num_runs):
+        # Flip a coin for an arrival  
+        isArrival = np.random.binomial(1, min(1, arrRate * del_t))
+        # Flip a coin for departure only if there is atleast one customer
+        if stateHistory[i-1] >= 1:
+            isDeparture = np.random.binomial(1, min(1, depRate * del_t))
+        else:
+            isDeparture = 0
+
+        # Update the current state
+        stateHistory[i] = stateHistory[i-1] + isArrival - isDeparture
+        
+        # Increment the timers for both inter-arrival and inter-departure
+        arrTimer += del_t
+        depTimer += del_t
+        # Increment the individual customer timers (for all those in the queue (incl. one in service))
+        for idx in range(customerID, len(individualTimers)):
+            individualTimers[idx] += del_t
+        
+        if isArrival:
+            intArrTimes.append(arrTimer)        # Record the inter-arrival timer reading
+            arrTimer = 0                        # Reset the timer for the same
+            individualTimers.append(0)          # Add a new timer for new customer
+        # Similarly for departures
+        if isDeparture:
+            intDepTimes.append(depTimer)        # Record the inter-departure timer reading
+            depTimer = 0                        # Reset the timer for the same
+            customerID += 1                     # Move on to the next customer.
+
+    return [stateHistory, intArrTimes, intDepTimes, individualTimers]
+
+def m_m_1_N_q(simTime, del_t, arrRate, depRate, N):
+    """ 
+    Function to simulate an M/M/1/N queue.
+    Just input 
+    1. simTime = Time duration (in seconds) that you would like to simulate the queue for.
+    2. arrRate = The arrival rate (per unit time)
+    3. depRate = The departure rate (per unit time)
+    4. N       = Buffer size
+    You will get as output 3 lists.
+    1. The state of the queue at time t = 0, delT, 2delT, 3delT, and so on.
+    2. The interarrival times.
+    3. The departure times.    
+    """ 
+    # To store the inter-arrival and inter-departure times
+    intArrTimes = []
+    intDepTimes = [] 
+    
+    # Timers for arrival and departure
+    arrTimer = 0 
+    depTimer = 0 
+
+    # Start with an empty queue
+    num_runs = int(simTime / del_t)
+    stateHistory = np.zeros(num_runs) 
+    
+    # Individual customer timer and tracking ID
+    individualTimers = []
+    customerID = 0                                
+
+    for i in range(1, num_runs):
+        # Flip a coin for arrival  
+        if stateHistory[i-1] >= N:
+            isArrival = 0                               # Turn down the new arrival(s)
+        else:
+            isArrival = np.random.binomial(1, min(1, arrRate * del_t))
+        # Flip a coin for departure only if there is atleast one customer
+        if stateHistory[i-1] >= 1:
+            isDeparture = np.random.binomial(1, min(1, depRate * del_t))
+        else:
+            isDeparture = 0
+
+        # Update the current state
+        stateHistory[i] = stateHistory[i-1] + isArrival - isDeparture
+
+        # Increment the timers for both arrival and departure
+        arrTimer += del_t
+        depTimer += del_t
+        for idx in range(customerID, len(individualTimers)):
+            individualTimers[idx] += del_t
+        
+        if isArrival:
+            intArrTimes.append(arrTimer)                # Record the interarrival time
+            arrTimer = 0                                # Reset the timer for the same
+            individualTimers.append(0)                  # Add a timer for the new customer
+        # Similarly for departures
+        if isDeparture:
+            intDepTimes.append(depTimer)                # Record the interdeparture time
+            depTimer = 0                                # Reset the timer for the same
+            customerID += 1                             # Take on the next customer for service.
+
+    return [stateHistory, intArrTimes, intDepTimes, individualTimers]
+
+def m_m_infinite_q(simTime, del_t, arrRate, depRate):
+    """ 
+    Function to simulate an M/M/infinite queue.
+    Just input 
+    1. simTime = Time duration (in seconds) that you would like to simulate the queue for.
+    2. arrRate = The arrival rate (per unit time)
+    3. depRate = The departure rate (per unit time)
+    
+    You will get as output 3 lists.
+    1. The state of the queue at time t = 0, delT, 2delT, 3delT, and so on.
+    2. The interarrival times.
+    3. The departure times.    
+    """ 
+    # To store the inter-arrival and inter-departure times
+    intArrTimes = []
+    intDepTimes = [] 
+    
+    # Timers for arrival and departure
+    arrTimer = 0 
+    depTimer = 0 
+
+    # Start with an empty queue
+    num_runs = int(simTime / del_t)
+    stateHistory = np.zeros(num_runs) 
+    
+    # Individual customer timer and tracking ID
+    individualTimers = []
+    customerID = 0                                  # For tracking purposes
+    activeIDs = []                                  # Will keep a record of customers currently at the server desk
+
+    for i in range(1, num_runs):
+        # Flip a coin for arrival
+        # print(f'--------------Run Begin-------------------------')  
+        # print(f'Previous state ={numCustomers[i-1]}')
+        isArrival = np.random.binomial(1, min(1, arrRate * del_t))
+        # print(f'isArrival = {isArrival}')
+        # Flip a coin for departure only if there is atleast 1 customer
+        if stateHistory[i-1] >= 1:
+            # Create a departures list of indices
+            isDepartures = [np.random.binomial(1, min(1, depRate * del_t)) for _ in range(int(stateHistory[i-1]))]
+        else:
+            isDepartures = []
+        # print(f'isDepartures = {isDepartures}')
+        # Update the current state
+        stateHistory[i] = stateHistory[i-1] + isArrival - np.sum(isDepartures)
+        # print(f'Updated state = {numCustomers[i]}')
+        # Increment the timers for both arrival and departure
+        arrTimer += del_t
+        for idx in activeIDs:
+            individualTimers[idx] += del_t
+        
+        # Similarly for departures
+        if isDepartures:
+            intDepTimes.append(depTimer)
+            depTimer = 0 
+        # Keep only the unserved IDs in the active ID list
+        tempIDs = [activeIDs[i] for i in range(len(isDepartures)) if isDepartures[i] == 0]
+        activeIDs = tempIDs
+        # print(f'activeIDs_post_dep = {activeIDs}')
+        
+        # If there was an arrival record the Inter-arrival time and clear the timer (restart)
+        if isArrival:
+            intArrTimes.append(arrTimer)
+            arrTimer = 0
+            individualTimers.append(0)
+            activeIDs.append(customerID)
+            customerID += 1 
+        # print(f'activeIDs_post_arr = {activeIDs}')
+
+        # print(f'--------------------Run End---------------------------')
+    return [stateHistory, intArrTimes, intDepTimes, individualTimers]
+
+def m_m_m_q(simTime, del_t, arrRate, depRate, numServers):
+    """ 
+    Function to simulate an M/M/m queue.
+    Just input 
+    1. simTime = Time duration (in seconds) that you would like to simulate the queue for.
+    2. arrRate = The arrival rate (per unit time)
+    3. depRate = The departure rate (per unit time)
+    4. numServers = Number of servers in the system
+
+    You will get as output 3 lists.
+    1. The state of the queue at time t = 0, delT, 2delT, 3delT, and so on.
+    2. The interarrival times.
+    3. The departure times.    
+    """ 
+    # To store the inter-arrival and inter-departure times
+    intArrTimes = []
+    intDepTimes = [] 
+    
+    # Timers for arrival and departure
+    arrTimer = 0 
+    depTimer = 0 
+
+    # Start with an empty queue
+    num_runs = int(simTime / del_t)
+    stateHistory = np.zeros(num_runs) 
+    
+    # Individual customer timer and tracking ID
+    individualTimers = []
+    customerID = 0                                  # For tracking purposes
+    activeIDs = []                                  # Will keep a record of customers currently at the server desk
+    waitingIDs = []
+
+    for i in range(1, num_runs):
+        # Flip a coin for arrival
+        # print(f'--------------Run Begin-------------------------')  
+        # print(f'Previous state ={numCustomers[i-1]}')
+        isArrival = np.random.binomial(1, min(1, arrRate * del_t))
+        # print(f'isArrival = {isArrival}')
+        # Flip a coin for departure only if there is atleast 1 customer
+        if stateHistory[i-1] >= 1:
+            # Create a departures list of indices
+            isDepartures = [np.random.binomial(1, min(1, depRate * del_t)) for _ in range(len(activeIDs))]
+        else:
+            isDepartures = []
+        # print(f'isDepartures = {isDepartures}')
+        # Update the current state
+        stateHistory[i] = stateHistory[i-1] + isArrival - np.sum(isDepartures)
+        # print(f'Updated state = {numCustomers[i]}')
+        # Increment the timers for both arrival and departure
+        arrTimer += del_t
+        for idx in activeIDs:
+            individualTimers[idx] += del_t
+        
+        # Similarly for departures
+        if isDepartures:
+            intDepTimes.append(depTimer)
+            depTimer = 0 
+        # Keep only the unserved IDs in the active ID list
+        tempIDs = [activeIDs[i] for i in range(len(isDepartures)) if isDepartures[i] == 0]
+        activeIDs = tempIDs
+        # print(f'activeIDs_post_dep = {activeIDs}')
+        
+        # If there was an arrival record the Inter-arrival time and clear the timer (restart)
+        if isArrival:
+            intArrTimes.append(arrTimer)
+            arrTimer = 0
+            individualTimers.append(0)
+            if len(activeIDs) == numServers :
+            # No free server :: Put the arrival in the waiting room 
+                waitingIDs.append(customerID)
+            else:
+            # Send to a free server
+                activeIDs.append(customerID)
+            customerID += 1 
+        # print(f'activeIDs_post_arr = {activeIDs}')
+
+        # print(f'--------------------Run End---------------------------')
+    return [stateHistory, intArrTimes, intDepTimes, individualTimers]
